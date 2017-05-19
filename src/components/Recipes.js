e6781267e3bca498fa5888786014de912b4fc01e
Colin Fendrick <colin.fendrick@gmail.com>
2017-05-19 12:31:36 -0400

    Updating recipe search

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -1,22 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 import TextField from 'material-ui/TextField'
 import Toggle from 'material-ui/Toggle'
 import { RecipeCard } from '.'
+import { get } from './api'
 
-const Recipes = () => (
-  <div className='Recipes'>
-    <div>
-      <TextField hintText='Search Recipes' />
-      <Toggle label='Show only foods within budget' />
-      <Toggle label='Vegetarian' />
+class Recipes extends Component {
+  state = {
+    entries: []
+  }
+  updateResults (query) {
+    get(query).then(r => this.setState({ entries: r.hits }))
+  }
+  recipeList = () => {
+    return this.state.entries.map(entry => <RecipeCard />)
+  }
+  render () {
+    return <div className='Recipes'>
+      <div>
+        <TextField hintText='Search Recipes' onChange={e => this.updateResults(e.target.value)} />
+        <Toggle label='Show only foods within budget' />
+        <Toggle label='Vegetarian' />
+      </div>
+      <div className='recipe-card-list'>
+        {this.recipeList}
+      </div>
     </div>
-    <div className='recipe-card-list'>
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-      <RecipeCard />
-    </div>
-  </div>
-)
+  }
+}
 
 export default Recipes
