acbb0b629b7976f707ac892ad4bb44a4c2a55e8c
Colin Fendrick <colin.fendrick@gmail.com>
2017-05-31 18:25:49 -0400

    Cleaned back up recipe card list

--- src/components/RecipeCardList.js
+++ src/components/RecipeCardList.js
@@ -3,14 +3,11 @@ import { RecipeCard } from '.'
 import store from '../store'
 import { observer } from 'mobx-react'
 
-const RecipeCardList = () => {
-  const list = store.entries ? store.entries.map((entry, i) =>
-    <RecipeCard entry={entry} key={i} />)
-  : <div />
-
-  return <div className='RecipeCardList'>
-    {list}
+const RecipeCardList = () => (
+  <div className='RecipeCardList'>
+    { store.entries.map((entry, i) =>
+      <RecipeCard entry={entry} key={i} />)}
   </div>
-}
+)
 
 export default observer(RecipeCardList)
