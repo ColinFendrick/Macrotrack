2b77eee95de73068559beaaf9a574136d6d9ccab
Colin Fendrick <colin.fendrick@gmail.com>
2017-06-06 15:08:56 -0400

    Updated with better pie chart

--- src/components/Log.js
+++ src/components/Log.js
@@ -1,68 +1,91 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Donut } from 'rebass'
 import store from '../store'
 import { observer } from 'mobx-react'
 import { LogItem } from '.'
-import PieChart from 'react-simple-pie-chart'
+import PieChart from 'react-svg-piechart'
 
-const Log = () => {
-  const d = store.daily
-  const t = store.total
-  const slices = [
+class Log extends Component {
+  state = {
+    expanded: null
+  }
+
+  data = [
     {
-      color: '#b33951',
-      value: t.protein
+      label: 'Protein',
+      value: 10,
+      color: '#b33951'
+
     },
     {
-      color: '#e3d081',
-      value: t.fats
+      label: 'Fats',
+      value: 10,
+      color: '#e3d081'
+
     },
     {
-      color: 'hsla(212, 68%, 40%, 0.58)',
-      value: t.carbs
+      label: 'Carbs',
+      value: 10,
+      color: 'hsla(212, 68%, 40%, 0.58)'
+
     }
   ]
 
-  return <div className='Log'>
-    <div className='log-top'>
-      <div className='log-cal'>
-        <br />
-        <Donut
-          color='hsl(118, 24%, 45%)'
-          size={256}
-          value={1 - ((d.calories - t.calories) / d.calories)}
+  _mouseEnter = sector => {
+    this.setState({expanded: sector})
+  }
+
+  render () {
+    const d = store.daily
+    const t = store.total
+
+    return <div className='Log'>
+      <div className='log-top'>
+        <div className='log-cal'>
+          <br />
+          <Donut
+            color='hsl(118, 24%, 45%)'
+            size={256}
+            value={1 - ((d.calories - t.calories) / d.calories)}
         />
-        <div className='calories'>Daily Calories</div>
+          <div className='calories'>Daily Calories</div>
+        </div>
+        <div style={{'width': '300px'}}>
+          <PieChart
+            data={this.data}
+            expandedSector={this.state.expanded}
+            onSectorHover={this._mouseEnter}
+          />
+        </div>
       </div>
-      <PieChart slices={slices} />
+      <div className='log-macro-donut'>
+        <div>
+          <Donut color='#b33951'
+            value={1 - ((d.protein - t.protein) / d.protein)} />
+          <div className='donut-label protein'>Protein</div>
+        </div>
+        <div>
+          <Donut color='hsla(212, 68%, 40%, 0.58)'
+            value={1 - ((d.carbs - t.carbs) / d.carbs)} />
+          <div className='donut-label carb'>Carbs</div>
+        </div>
+        <div>
+          <Donut color='#e3d081'
+            value={1 - ((d.fats - t.fats) / d.fats)} />
+          <div className='donut-label fat'>Fats</div>
+        </div>
+      </div>
+      <div className='daily-log'>
+        <div className='log-list'>
+          <LogItem meal='Breakfast' />
+          <LogItem meal='Lunch' />
+          <LogItem meal='Dinner' />
+          <LogItem meal='Snack' />
+        </div>
+      </div>
+      <div className='weekly-log' />
     </div>
-    <div className='log-macro-donut'>
-      <div>
-        <Donut color='#b33951'
-          value={1 - ((d.protein - t.protein) / d.protein)} />
-        <div className='donut-label protein'>Protein</div>
-      </div>
-      <div>
-        <Donut color='hsla(212, 68%, 40%, 0.58)'
-          value={1 - ((d.carbs - t.carbs) / d.carbs)} />
-        <div className='donut-label carb'>Carbs</div>
-      </div>
-      <div>
-        <Donut color='#e3d081'
-          value={1 - ((d.fats - t.fats) / d.fats)} />
-        <div className='donut-label fat'>Fats</div>
-      </div>
-    </div>
-    <div className='daily-log'>
-      <div className='log-list'>
-        <LogItem meal='Breakfast' />
-        <LogItem meal='Lunch' />
-        <LogItem meal='Dinner' />
-        <LogItem meal='Snack' />
-      </div>
-    </div>
-    <div className='weekly-log' />
-  </div>
+  }
 }
 
 export default observer(Log)
